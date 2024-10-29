85b2bcdb4725c576811df51034881d02f2458f08
Joshi1990-code17 <jay.joshi1200@gmail.com>
2024-10-29 09:22:09 -0400

    yo

---
+++ academy/src/test/java/academy/Demo1.java
@@ -0,0 +1,18 @@
+package academy;
+
+import java.util.Scanner;
+
+public class Demo1 {
+
+	
+		public static void main ( String [] args ) {
+			
+			Scanner sc = new Scanner(System.in);
+			
+			String name = sc.next();
+			
+			String input = "Enter your name "+ name ;
+			
+			System.out.println(input);
+		}
+}
